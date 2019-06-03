deed003560283fb025a00bbfd745edff85710769
shenae <shhnae@gmail.com>
2019-06-03 15:14:01 -0400

    updated readme and wireframes

--- blessed_be_the_pantsuit/config/application.rb
+++ blessed_be_the_pantsuit/config/application.rb
@@ -26,6 +26,12 @@ module BlessedBeThePantsuit
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.middleware.use Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :put, :options, :delete], :credentials => false
+      end
+    end
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
